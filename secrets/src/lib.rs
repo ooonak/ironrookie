4030a05daea195555934a96dda9c6d211953a4c0
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-02-20 16:03:16 +0100

    Added cergo checkmate config.

--- secrets/src/lib.rs
+++ secrets/src/lib.rs
@@ -1,23 +1,28 @@
 mod signing;
 
-extern crate serde;
 extern crate rmp_serde as rmps;
+extern crate serde;
 
 use rand::Rng;
-use serde::{Deserialize, Serialize};
 use rustflake::Snowflake;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Message {
     msg_id: i64,
     beacon_id: i64,
     nonce: i32,
-    cmd: String
+    cmd: String,
 }
 
 impl Message {
     pub fn new(beacon: i64) -> Self {
         let mut rng = rand::thread_rng();
-        Self { msg_id: Snowflake::default().generate(), beacon_id: beacon, nonce: rng.gen::<i32>(), cmd: "Hello".to_string() }
+        Self {
+            msg_id: Snowflake::default().generate(),
+            beacon_id: beacon,
+            nonce: rng.gen::<i32>(),
+            cmd: "Hello".to_string(),
+        }
     }
 }
